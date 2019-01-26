b3436c80b49a7ffcbdf5571e0c4a960f1bf4edbd
Vincent Jacques <vincent@vincent-jacques.net>
2019-01-26 10:43:11 +0100

    Remove useless comment

--- sources/providers.tf
+++ sources/providers.tf
@@ -1,7 +1,5 @@
 variable "gandi_api_key" {}
 
-# go get github.com/tiramiseb/terraform-provider-gandi
-# ln -sf $HOME/go/bin/terraform-provider-gandi .terraform/plugins/linux_amd64
 provider "gandi" {
   key = "${var.gandi_api_key}"
 }
