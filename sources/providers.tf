1b9055e6b610403475689fa8c6029e58938a7ef7
Vincent Jacques <vincent@vincent-jacques.net>
2019-01-31 08:09:52 +0100

    Move comments to an un-encrypted file
    Encrypted comments wouldn't help much for disaster recovery

--- sources/providers.tf
+++ sources/providers.tf
@@ -1,12 +1,16 @@
+# https://account.gandi.net/fr/users/jacquev6/security
 variable "gandi_api_key" {}
 
 provider "gandi" {
   key = "${var.gandi_api_key}"
 }
 
+# https://console.cloud.google.com/iam-admin/serviceaccounts/details/113793464457417850819?project=jacquev6-0001
 provider "google" {
   credentials = "${file("gcp-account.json")}"
   project = "jacquev6-0001"
   region = "europe-west1"
   zone = "europe-west1-c"
 }
+
+# https://console.aws.amazon.com/iam/home?#/users/infrastructure-as-code
