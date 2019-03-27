9523387f746a85115e1829c58ece60ba7e1453fe
Vincent Jacques <vincent@vincent-jacques.net>
2019-03-27 10:41:46 +0100

    Add dummy CRUD functions to support 'terraform apply'

--- infrastructure-tools/terraform-provider-sfrbox/terraform-provider-sfrbox.go
+++ infrastructure-tools/terraform-provider-sfrbox/terraform-provider-sfrbox.go
@@ -28,11 +28,28 @@ func main() {
               "mac": {
                 Type: schema.TypeString,
                 Required: true,
+                ForceNew: true,
               },
               "ip": {
                 Type: schema.TypeString,
                 Required: true,
+                ForceNew: true,
               },
+            },
+
+            Create: func (d *schema.ResourceData, m interface{}) error {
+              mac := d.Get("mac").(string)
+              d.SetId(mac)
+              return nil
+            },
+            Read: func (d *schema.ResourceData, m interface{}) error {
+              return nil
+            },
+            Delete: func (d *schema.ResourceData, m interface{}) error {
+              return nil
+            },
+            Exists: func (d *schema.ResourceData, m interface{}) (bool, error) {
+              return true, nil
             },
           },
         },
