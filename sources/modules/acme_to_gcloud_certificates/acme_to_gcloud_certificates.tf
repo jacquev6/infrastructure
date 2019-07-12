45632628bb0ad1155d21ccf666ccb2e3cc0b5632
Vincent Jacques <vincent@vincent-jacques.net>
2019-07-12 09:19:48 +0200

    Renew all SSL certificates

--- sources/modules/acme_to_gcloud_certificates/acme_to_gcloud_certificates.tf
+++ sources/modules/acme_to_gcloud_certificates/acme_to_gcloud_certificates.tf
@@ -7,6 +7,7 @@ variable "acme_account_key_pem" {}
 resource "acme_certificate" "certificate" {
   account_key_pem = "${var.acme_account_key_pem}"
   common_name = "${var.domain_name}"
+  min_days_remaining = "20"  # To match ACME's e-mail reminder
 
   dns_challenge {
     provider = "gandiv5"
