9cd0504fa40de83660c9439d1046cb8ba3f33a8d
Vincent Jacques <vincent@vincent-jacques.net>
2020-02-12 19:21:40 +0100

    Fix idempotence issue (container recreated on all "infra apply"s)

--- sources/resources/doorman_containers/doorman_containers.tf
+++ sources/resources/doorman_containers/doorman_containers.tf
@@ -1,6 +1,10 @@
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
 resource "docker_container" "always_200" {
   name  = "always_200"
-  image = "nginx:latest"
+  image = "${docker_image.nginx.latest}"  # Don't simply use "nginx:latest" here: it triggers a new container on every "infra apply"
   rm = "false"
   restart = "always"
   ports {
