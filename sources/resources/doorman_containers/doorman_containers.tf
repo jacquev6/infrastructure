fbe123028a5ee79d1d9d89d20222ca302c87b884
Vincent Jacques <vincent@vincent-jacques.net>
2020-02-13 15:38:49 +0100

    Comment

--- sources/resources/doorman_containers/doorman_containers.tf
+++ sources/resources/doorman_containers/doorman_containers.tf
@@ -16,6 +16,12 @@ resource "docker_container" "always_200" {
 
 resource "docker_image" "draw_turks_head_demo" {
   name = "jacquev6/draw-turks-head-demo:20200213-135841"
+  # It's very weird that changing name does not trigger a new resource.
+  # As a result, without pull_triggers, we need to run "infra apply" twice
+  # after building a new version of draw_turks_head_demo.
+  # This might be a bug in the provider.
+  # @todo (after upgrading to latest terraform and provider versions) Remove the pull_triggers workaround and test if changing the name does replace the associated container.
+  # If not, open an issue on https://github.com/terraform-providers/terraform-provider-docker.
   pull_triggers = ["jacquev6/draw-turks-head-demo:20200213-135841"]
 }
 
