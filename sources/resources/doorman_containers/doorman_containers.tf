d7533fc0c35c9dc68670ab0c4e97b7798b476b7a
Vincent Jacques <vincent@vincent-jacques.net>
2020-02-14 11:18:34 +0100

    Todo

--- sources/resources/doorman_containers/doorman_containers.tf
+++ sources/resources/doorman_containers/doorman_containers.tf
@@ -7,6 +7,13 @@ variable "wildcard_vjnet_crt" {}
 
 resource "docker_image" "nginx" {
   name = "nginx:1.17-alpine"
+  # It's very weird that changing name does not trigger a new resource.
+  # As a result, without pull_triggers, we need to run "infra apply" twice
+  # after building a new version of draw_turks_head_demo.
+  # This might be a bug in the provider. This might be fixed in more recent versions of Terraform and/or the provider.
+  # @todo (after upgrading to latest terraform and provider versions) Remove the pull_triggers workaround and test if changing the name does replace the associated container.
+  # If not, open an issue on https://github.com/terraform-providers/terraform-provider-docker.
+  pull_triggers = ["nginx:1.17-alpine"]
 }
 
 data "local_file" "always_200_nginx_conf" {
@@ -32,6 +39,7 @@ resource "docker_container" "always_200" {
   image = "${docker_image.nginx.latest}"  # Don't simply use "nginx:latest" here: it triggers a new container on every "infra apply"
   rm = "false"
   restart = "always"
+  # @todo readonly = "true"
   ports {
     internal = "80"
     external = "80"
@@ -69,12 +77,6 @@ resource "docker_container" "always_200" {
 
 resource "docker_image" "draw_turks_head_demo" {
   name = "jacquev6/draw-turks-head-demo:20200213-135841"
-  # It's very weird that changing name does not trigger a new resource.
-  # As a result, without pull_triggers, we need to run "infra apply" twice
-  # after building a new version of draw_turks_head_demo.
-  # This might be a bug in the provider. This might be fixed in more recent versions of Terraform and/or the provider.
-  # @todo (after upgrading to latest terraform and provider versions) Remove the pull_triggers workaround and test if changing the name does replace the associated container.
-  # If not, open an issue on https://github.com/terraform-providers/terraform-provider-docker.
   pull_triggers = ["jacquev6/draw-turks-head-demo:20200213-135841"]
 }
 
@@ -84,6 +86,7 @@ resource "docker_container" "draw_turks_head_demo" {
   rm = "false"
   restart = "always"
   # @todo readonly = "true"
+  # @todo Use a Docker network, don't publish this port
   ports {
     internal = "80"
     external = "8081"
