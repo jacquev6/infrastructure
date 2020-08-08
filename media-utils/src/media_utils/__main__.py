d9032d2d6db1832213d9877d85639bab088d17ba
Vincent Jacques <vincent@vincent-jacques.net>
2020-08-08 10:53:42 +0200

    On the way to ripping DVDs

--- media-utils/src/media_utils/__main__.py
+++ media-utils/src/media_utils/__main__.py
@@ -22,25 +22,29 @@ def main(redis_url):
 
 @main.command()
 @click.argument("devices", nargs=-1)
-def chain_rip(devices):
+@click.argument("data_path", nargs=1)
+def chain_rip(devices, data_path):
     print("Chain ripping from the following devices:", " ".join(devices), flush=True)
 
     rip_dvds = rq.Queue(name="rip-dvds", connection=redis_connection)
 
     while True:
-        print("Polling loop at", datetime.datetime.now(), flush=True)
+        now = datetime.datetime.now()
+        print("Polling loop at", now, flush=True)
         devices_being_ripped = set(job.args[0] for job in rip_dvds.get_jobs())
         print("Devices being ripped:", " ".join(devices_being_ripped))
         for device in devices:
-            if device not in devices_being_ripped and has_disk(device):
-                print("Ripping from", device)
-                rip_dvds.enqueue(rip_dvd, device)
+            if device not in devices_being_ripped:
+                if has_disk(device):
+                    print("Ripping from", device)
+                    rip_dvds.enqueue(rip_dvd, device, os.path.join(data_path, "new"), now)
 
         print(flush=True)
         time.sleep(10)
 
 
 def has_disk(device):
+    # https://superuser.com/a/1367091/517309
     fd = os.open(device, os.O_RDONLY | os.O_NONBLOCK)
     try:
         return fcntl.ioctl(fd, 0x5326) == 4
--- media-utils/src/media_utils/rip_dvd.py
+++ media-utils/src/media_utils/rip_dvd.py
@@ -1,5 +1,30 @@
+import os
+import datetime
+import subprocess
+
+import random
 import time
 
 
-def rip_dvd(device):
-    time.sleep(25)
+def rip_dvd(device: str, output_directory_path: str, now: datetime.datetime):
+    os.makedirs(output_directory_path, exist_ok=True)
+
+    isoinfo = subprocess.run(
+        ["isoinfo", "-d", "-i", device],
+        check=True,
+        capture_output=True,
+        universal_newlines=True
+    ).stdout.splitlines()
+    assert isoinfo[0] == "CD-ROM is in ISO 9660 format"
+    assert isoinfo[2].startswith("Volume id: ")
+    volume_id = isoinfo[2][11:]
+
+    iso_path = os.path.join(output_directory_path, f"{volume_id}-{now.strftime('%Y%m%d-%H%M%S')}-{os.path.basename(device)}.iso")
+    tmp_path = iso_path + ".part"
+
+    # subprocess.run(["dvdvideo-backup-image", device, tmp_path], check=True)
+    with open(tmp_path, "w"):
+        pass
+    time.sleep(15 + random.randint(0, 20))
+
+    os.rename(tmp_path, iso_path)
