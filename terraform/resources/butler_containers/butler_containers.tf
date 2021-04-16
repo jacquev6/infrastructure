becc305249481e6b3a1fee4ba1d0bc2293155fad
Vincent Jacques <vincent@vincent-jacques.net>
2021-04-16 07:25:02 +0200

    Clean-up Butler (4/?)

--- terraform/resources/butler_containers/butler_containers.tf
+++ terraform/resources/butler_containers/butler_containers.tf
@@ -4,13 +4,3 @@ variable "certificates" {
     crt = string
   }))
 }
-
-data "docker_registry_image" "nginx" {
-  name = "nginx:1.17-alpine"
-}
-
-resource "docker_image" "nginx" {
-  name = data.docker_registry_image.nginx.name
-  pull_triggers = [data.docker_registry_image.nginx.sha256_digest]
-  keep_locally = true
-}
