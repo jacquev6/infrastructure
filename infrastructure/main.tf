f1c2a3513c3fb167204bc1635f3acdb70d33f96d
Vincent Jacques <vincent@vincent-jacques.net>
2023-03-06 13:33:25 +0100

    Go deeper in S3 bucket configuration

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,6 +3,7 @@ terraform {
 
   required_providers {
     aws = {
+      # https://registry.terraform.io/providers/hashicorp/aws/4.57.0/docs
       source = "hashicorp/aws"
       version = "~> 4.57"
     }
@@ -28,7 +29,46 @@ provider "aws" {
 # Tweaked using 'terraform plan' to match the current state.
 resource "aws_s3_bucket" "jacquev6" {
   bucket = "jacquev6"
+
+  tags = {}  # Tab "Properties", section "Tags"
 }
+
+# See https://github.com/hashicorp/terraform-provider-aws/issues/23106 for more
+# information about the plethora of additional resources required to configure a
+# S3 bucket.
+
+# Tab "Properties", section "Bucket Versioning"
+resource "aws_s3_bucket_versioning" "jacquev6" {
+  bucket = aws_s3_bucket.jacquev6.id
+
+  versioning_configuration {
+    status = "Disabled"
+  }
+}
+
+# Tab "Properties", section "Default encryption"
+resource "aws_s3_bucket_server_side_encryption_configuration" "jacquev6" {
+  bucket = aws_s3_bucket.jacquev6.id
+
+  rule {
+    bucket_key_enabled = false
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+# Tab "Properties", section "Requester pays"
+resource "aws_s3_bucket_request_payment_configuration" "jacquev6" {
+  bucket = aws_s3_bucket.jacquev6.id
+  payer = "Requester"
+}
+
+# Tab "Properties", section "Website hosting"
+# No resource "aws_s3_bucket_website_configuration"
+
+
+# Tab "Permissions", section "Block public access (bucket settings)"
 resource "aws_s3_bucket_public_access_block" "jacquev6" {
   bucket = aws_s3_bucket.jacquev6.id
 
@@ -37,3 +77,49 @@ resource "aws_s3_bucket_public_access_block" "jacquev6" {
   ignore_public_acls = true
   restrict_public_buckets = true
 }
+
+
+# Tab "Management", section "Lifecycle rules"
+# No resource "aws_s3_bucket_lifecycle_configuration"
+
+# Tab "Management", section "Replication rules"
+# No resource "aws_s3_bucket_replication_configuration"
+
+# Tab "Management", section "Inventory configurations"
+# No resource "aws_s3_bucket_inventory"
+
+
+# Not yet investigated
+# resource "aws_s3_bucket_intelligent_tiering_configuration" "jacquev6" {
+#   bucket = aws_s3_bucket.jacquev6.id
+# }
+# resource "aws_s3_bucket_logging" "jacquev6" {
+#   bucket = aws_s3_bucket.jacquev6.id
+# }
+# resource "aws_s3_bucket_accelerate_configuration" "jacquev6" {
+#   bucket = aws_s3_bucket.jacquev6.id
+# }
+# resource "aws_s3_bucket_acl" "jacquev6" {
+#   bucket = aws_s3_bucket.jacquev6.id
+# }
+# resource "aws_s3_bucket_analytics_configuration" "jacquev6" {
+#   bucket = aws_s3_bucket.jacquev6.id
+# }
+# resource "aws_s3_bucket_cors_configuration" "jacquev6" {
+#   bucket = aws_s3_bucket.jacquev6.id
+# }
+# resource "aws_s3_bucket_metric" "jacquev6" {
+#   bucket = aws_s3_bucket.jacquev6.id
+# }
+# resource "aws_s3_bucket_notification" "jacquev6" {
+#   bucket = aws_s3_bucket.jacquev6.id
+# }
+# resource "aws_s3_bucket_object_lock_configuration" "jacquev6" {
+#   bucket = aws_s3_bucket.jacquev6.id
+# }
+# resource "aws_s3_bucket_ownership_controls" "jacquev6" {
+#   bucket = aws_s3_bucket.jacquev6.id
+# }
+# resource "aws_s3_bucket_policy" "jacquev6" {
+#   bucket = aws_s3_bucket.jacquev6.id
+# }
