e0761a86411f94249676a8636c234f4016405920
Vincent Jacques <vincent@vincent-jacques.net>
2023-03-10 08:25:28 +0100

    Move secrets into dedicated directory

--- shell/run.sh
+++ shell/run.sh
@@ -6,16 +6,20 @@ set -o pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 
-if [[ ! -f shell/secrets.env ]]; then
-  echo "Please create shell/secrets.env according to shell/secrets.env.template"
-  exit 1
-fi
+secrets_ok=true
+for template in secrets/*.template; do
+  if [[ ! -f "${template%.template}" ]]; then
+    echo "Please create ${template%.template} according to $template"
+    secrets_ok=false
+  fi
+done
+$secrets_ok
 
 docker build shell --tag cloud-infrastructure-shell
 
 docker run \
   --rm --interactive --tty \
   --volume "$PWD:/wd" --workdir /wd/infrastructure \
-  --env-file shell/secrets.env \
+  --env-file secrets/env \
   cloud-infrastructure-shell \
     "$@"
