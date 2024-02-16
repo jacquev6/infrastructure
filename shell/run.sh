eb9dcaa74cd90415f818a3f69aaceaa1cef118f9
Vincent Jacques <vincent@vincent-jacques.net>
2024-02-16 07:27:13 +0000

    Settle on a method for SSH configuration files

--- shell/run.sh
+++ shell/run.sh
@@ -7,21 +7,27 @@ cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
 
 secrets_ok=true
-for template in secrets/*.template; do
-  if [[ ! -f "${template%.template}" ]]; then
-    echo "Please create ${template%.template} according to $template"
+echo "# Secrets" > .gitignore
+find . -name '*.template' | while read template; do
+  secret="${template%.template}"
+  secret="${secret#./}"
+  echo "$secret" >> .gitignore
+  if [[ ! -f "$secret" ]]; then
+    echo "Please create $secret according to $template"
     secrets_ok=false
   fi
 done
 $secrets_ok
 
-docker build shell --tag cloud-infrastructure-shell
+chmod 600 infrastructure/configuration/ssh/id_rsa
+
+user_name=$(whoami)
+user_id=$(id -u)
+docker build --build-arg USER_NAME=$user_name --build-arg USER_ID=$user_id shell --tag infrastructure-shell
 
 docker run \
   --rm --interactive --tty \
-  --volume "$PWD/secrets/main.id_rsa:/root/.ssh/id_rsa:ro" \
-  --volume "$PWD/shell/ssh_known_hosts:/root/.ssh/known_hosts" \
-  --volume "$PWD:/project" \
-  --workdir /project \
-  --env-file secrets/env \
-  cloud-infrastructure-shell "$@"
+  --volume "$PWD/infrastructure/configuration/ssh/:/home/$user_name/.ssh/" \
+  --volume "$PWD:/project" --workdir /project \
+  --env-file infrastructure/provisioning/credentials.env \
+  infrastructure-shell "$@"
